00d0f36ccd3cf5541aa67bae96115110b1a9ea25
Yuan Kaiwen <joeyscave@outlook.com>
2023-03-24 17:23:15 +0800

    :new: arp

--- src/arp.c
+++ src/arp.c
@@ -5,7 +5,7 @@
 #include "ethernet.h"
 /**
  * @brief 初始的arp包
- * 
+ *
  */
 static const arp_pkt_t arp_init_pkt = {
     .hw_type16 = constswap16(ARP_HW_ETHER),
@@ -18,19 +18,19 @@ static const arp_pkt_t arp_init_pkt = {
 
 /**
  * @brief arp地址转换表，<ip,mac>的容器
- * 
+ *
  */
 map_t arp_table;
 
 /**
  * @brief arp buffer，<ip,buf_t>的容器
- * 
+ *
  */
 map_t arp_buf;
 
 /**
  * @brief 打印一条arp表项
- * 
+ *
  * @param ip 表项的ip地址
  * @param mac 表项的mac地址
  * @param timestamp 表项的更新时间
@@ -42,7 +42,7 @@ void arp_entry_print(void *ip, void *mac, time_t *timestamp)
 
 /**
  * @brief 打印整个arp表
- * 
+ *
  */
 void arp_print()
 {
@@ -53,51 +53,116 @@ void arp_print()
 
 /**
  * @brief 发送一个arp请求
- * 
+ *
  * @param target_ip 想要知道的目标的ip地址
  */
 void arp_req(uint8_t *target_ip)
 {
-    // TO-DO
+    // 调用buf_init()对txbuf进行初始化
+    buf_init(&txbuf, sizeof(arp_pkt_t));
+
+    // 填写arp报头
+    arp_pkt_t arp_head = arp_init_pkt;
+    // ARP操作类型为ARP_REQUEST，注意大小端转换
+    arp_head.opcode16 = swap16(ARP_REQUEST);
+    // 填写目的ip
+    memcpy(arp_head.target_ip, target_ip, NET_IP_LEN);
+    // 装入发送缓冲区
+    memcpy(txbuf.data, &arp_head, sizeof(arp_pkt_t));
+    // 调用ethernet_out函数将ARP报文发送出去
+    ethernet_out(&txbuf, ether_broadcast_mac, NET_PROTOCOL_ARP);
 }
 
 /**
  * @brief 发送一个arp响应
- * 
+ *
  * @param target_ip 目标ip地址
  * @param target_mac 目标mac地址
  */
 void arp_resp(uint8_t *target_ip, uint8_t *target_mac)
 {
-    // TO-DO
+    // 调用buf_init()对txbuf进行初始化
+    buf_init(&txbuf, sizeof(arp_pkt_t));
+    // 填写arp报头
+    arp_pkt_t arp_head = arp_init_pkt;
+    // ARP操作类型为ARP_REPLY，注意大小端转换
+    arp_head.opcode16 = swap16(ARP_REPLY);
+    // 填写目的ip
+    memcpy(arp_head.target_ip, target_ip, NET_IP_LEN);
+    // 填写目的mac
+    memcpy(arp_head.target_mac, target_mac, NET_MAC_LEN);
+    // 装入发送缓冲区
+    memcpy(txbuf.data, &arp_head, sizeof(arp_pkt_t));
+    // 调用ethernet_out()函数将填充好的ARP报文发送出去
+    ethernet_out(&txbuf, target_mac, NET_PROTOCOL_ARP);
 }
 
 /**
  * @brief 处理一个收到的数据包
- * 
+ *
  * @param buf 要处理的数据包
  * @param src_mac 源mac地址
  */
 void arp_in(buf_t *buf, uint8_t *src_mac)
 {
-    // TO-DO
+    if (buf->len < sizeof(arp_pkt_t))
+        return;
+
+    arp_pkt_t *arp_pkt = (arp_pkt_t *)buf->data;
+    if (arp_pkt->hw_type16 != constswap16(ARP_HW_ETHER))
+        return;
+    if (arp_pkt->pro_type16 != constswap16(NET_PROTOCOL_IP))
+        return;
+    if (arp_pkt->hw_len != NET_MAC_LEN)
+        return;
+    if (arp_pkt->pro_len != NET_IP_LEN)
+        return;
+    if (arp_pkt->opcode16 != swap16(ARP_REQUEST) && arp_pkt->opcode16 != swap16(ARP_REPLY))
+        return;
+
+    map_set(&arp_table, arp_pkt->sender_ip, src_mac);
+    buf_t *uncommit_buf = map_get(&arp_buf, arp_pkt->sender_ip);
+    if (uncommit_buf != NULL)
+    {
+        ethernet_out(uncommit_buf, src_mac, NET_PROTOCOL_IP);
+        map_delete(&arp_buf, arp_pkt->sender_ip);
+    }
+    if (arp_pkt->opcode16 == swap16(ARP_REQUEST) && !memcmp(arp_pkt->target_ip, net_if_ip, NET_IP_LEN))
+    {
+        arp_resp(arp_pkt->sender_ip, src_mac);
+    }
 }
 
 /**
  * @brief 处理一个要发送的数据包
- * 
+ *
  * @param buf 要处理的数据包
  * @param ip 目标ip地址
  * @param protocol 上层协议
  */
 void arp_out(buf_t *buf, uint8_t *ip)
 {
-    // TO-DO
+    if (map_get(&arp_table, ip) == NULL)
+    {
+        if (map_get(&arp_buf, ip) == NULL)
+        {
+            map_set(&arp_buf, ip, buf);
+            arp_req(ip);
+        }
+        else
+        {
+            // TODO:防止丢包处理
+        }
+    }
+    else
+    {
+        ethernet_out(buf, map_get(&arp_table, ip), NET_PROTOCOL_IP);
+    }
 }
 
 /**
  * @brief 初始化arp协议
- * 
+ *
  */
 void arp_init()
 {
